e24fa2f3359e866413cd11ff7d4378605b7319be
janik.buecher-cic@ibm.com <janik.buecher-cic@ibm.com>
2021-11-15 16:57:35 +0100

    Update version

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-        <h1> Version 1.0 </h1>
+        <h1> Version 1.1 </h1>
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
